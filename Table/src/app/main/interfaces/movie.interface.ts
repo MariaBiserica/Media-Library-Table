137b84a4e92d67b3b5f1193bb277c429526e6830
MariaBiserica <mariabiserica@gmail.com>
2023-04-25 17:21:51 +0300

    Implemented basic movie table: 3 components (dashboard, header, table), a service for data manipulation, movie interface; and stylized the appearance of the table.

---
+++ Table/src/app/main/interfaces/movie.interface.ts
@@ -0,0 +1,11 @@
+export interface Movie {
+  title: string;
+  year: number;
+  runtime: string;
+  genre: string;
+  actors: string;
+  plot: string;
+  awards: string;
+  poster: string;
+  imdbRating: string;
+}
